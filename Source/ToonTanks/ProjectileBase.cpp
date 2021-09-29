03dd3c3fc1ee9891610d95432cc8b994894b6999
HOME\ruanl <scorpiorl5811@gmail.com>
2021-09-29 16:45:07 +0800

    Add ProjectileBase

--- Source/ToonTanks/ProjectileBase.cpp
+++ Source/ToonTanks/ProjectileBase.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Actors/ProjectileBase.h"
+#include "ProjectileBase.h"
 
 // Sets default values
 AProjectileBase::AProjectileBase()
